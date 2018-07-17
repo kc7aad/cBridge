0c07aaf00d575fb3fe5bec47b18e0e70e2bd95eb
kc7aad <kc7aad@Suzy-1.kc7aad.com>
2018-07-17 06:38:51 -0700

    Second commit

--- netwatch.sh
+++ netwatch.sh
@@ -1,10 +1,16 @@
+# This file is being tested to collect and archive netwatch traffic from the cBridge.
+# This file is a work in progress, and may be removed due to being a waste of time! 
+#
+#
+#
+
 #!/bin/bash
 
 echo Beginning...
-#sudo cp /home/kc7aad/watcher /home/kc7aad/watcher.old
-sudo wget -O /home/kc7aad/watcher http://44.12.9.10:42420/report?netwatch
-sudo cat /home/kc7aad/watcher /home/kc7aad/watcher.old > /home/kc7aad/combine
-sudo sort -r -t " " -k2.1,2.6 -k1.1,1.9 -u /home/kc7aad/combine > /home/kc7aad/diff
+#sudo cp ~/watcher ~/watcher.old
+sudo wget -O ~/watcher http://44.12.9.10:42420/report?netwatch
+sudo cat ~/watcher ~/watcher.old > ~/combine
+sudo sort -r -t " " -k2.1,2.6 -k1.1,1.9 -u ~/combine > ~/diff
 sudo cp diff /var/www/html/pnw.txt
 echo Cleanup!!
 #sudo rm diff
