76c326b3acb0fdf7066eb10a0cdd3fc9a8cbaa42
kc7aad <35578844+kc7aad@users.noreply.github.com>
2019-01-19 23:40:27 -0800

    Update user_import.sh

--- user_import.sh
+++ user_import.sh
@@ -4,7 +4,7 @@ echo =======================================Users===============================
 echo $dt >> /var/log/ids.txt
 wget -O /script/PNW_Contacts.txt http://script.kc7aad.com/PNW_Contacts.txt
 while read cbridge
-do python /home/kc7aad/upload_ids.py -c /script/PNW_Contacts.txt -r -u PNWScript -d PNWskrypt $cbridge >> /var/log/ids.txt
+do python /home/kc7aad/upload_ids.py -c /script/PNW_Contacts.txt -r -u <USERNAME> -d <PASSWORD> $cbridge >> /var/log/ids.txt
 done < /script/cbridges.txt
 wc -l /script/PNW_Contacts.txt >> /var/log/ids.txt
 echo =======================================Users======================================= >> /var/log/ids.txt
