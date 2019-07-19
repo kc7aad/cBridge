72e7bf32b44474864662c679b0d31a0ae313953b
kc7aad <35578844+kc7aad@users.noreply.github.com>
2019-07-19 09:57:10 -0700

    Update user_import.sh

--- user_import.sh
+++ user_import.sh
@@ -2,10 +2,13 @@
 dt=`date '+%D %T'`
 echo =======================================Users======================================= >> /var/log/ids.txt
 echo $dt >> /var/log/ids.txt
-wget -O /script/PNW_Contacts.txt http://script.kc7aad.com/PNW_Contacts.txt
+curl http://script.kc7aad.com/US_Contacts.txt > /script/tmp/users.txt
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ >> /scr
+ipt/tmp/users.txt
+curl http://www.pnwdigital.net/Admin/Radio-IDs-PNW-Custom-Load-Last.txt >> /script/tmp/users.txt
 while read cbridge
-do python /home/kc7aad/upload_ids.py -c /script/PNW_Contacts.txt -r -u <USERNAME> -d <PASSWORD> $cbridge >> /var/log/ids.txt
+do python /home/kc7aad/upload_ids.py -c /script/tmp/users.txt -r -u PNWScript -d PNWskrypt $cbridge >> /var/log/ids.txt
 done < /script/cbridges.txt
-wc -l /script/PNW_Contacts.txt >> /var/log/ids.txt
+wc -l /script/tmp/users.txt >> /var/log/ids.txt
 echo =======================================Users======================================= >> /var/log/ids.txt
-rm /script/PNW_Contacts.txt
+rm /script/tmp/*.txt
