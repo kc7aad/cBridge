92c77677a39aa086c7a79f9657d727dc1fe68448
kc7aad <35578844+kc7aad@users.noreply.github.com>
2019-07-19 09:58:26 -0700

    Update peer_import.sh

--- peer_import.sh
+++ peer_import.sh
@@ -2,10 +2,13 @@
 dt=`date '+%D %T'`
 echo =======================================Peers======================================= >> /var/log/ids.txt
 echo $dt >> /var/log/ids.txt
-wget -O /script/cBridge_peers.txt http://script.kc7aad.com/cBridge_peers.txt
+curl http://script.kc7aad.com/Peers.txt > /script/tmp/peers.txt
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ >> /scr
+ipt/tmp/peers.txt
+curl http://www.pnwdigital.net/Admin/Peers-IDs-PNW-Custom-Load-Last.txt >> /script/tmp/peers.txt
 while read cbridge
-do python /home/kc7aad/upload_ids.py -c /script/cBridge_peers.txt -p -u <USERNAME> -d <PASSWORD> $cbridge >> /var/log/ids.txt
+do python /home/kc7aad/upload_ids.py -c /script/peers.txt -p -u PNWScript -d PNWskrypt $cbridge >> /var/log/ids.txt
 done < /script/cbridges.txt
-wc -l /script/cBridge_peers.txt >> /var/log/ids.txt
+wc -l /script/peers.txt >> /var/log/ids.txt
 echo =======================================Peers======================================= >> /var/log/ids.txt
-rm /script/cBridge_peers.txt
+rm /script/tmp/*.txt
